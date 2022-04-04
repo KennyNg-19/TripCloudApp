8ac71f141d1bd510d6e84d0d7d0929f88c7d0a32
kenny <wuyuhao2019@126.com>
2022-04-04 21:05:55 +0800

    add try-except

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from distutils.log import info
+from typing import final
 from utils import *
 import streamlit as st
 import pandas as pd
@@ -29,157 +30,168 @@ import geocoder
 
 # 标题
 st.title('TripElite 🚗 💨')
-st.subheader("- A platform for finding entertainment and carpark places.")
+st.subheader("- A platform for finding entertainment and carpark places around your destination.")
 
 # read data
 df = pd.read_csv("sg-places-new.csv").dropna()
 
 #! step0: input a 目的地, in Singapore to go
 st.write("### 0. Input a Destination in SG you plan to reach:")
-input_destination = st.text_input("A place name in SG, PRESS Enter!")
+
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent="example app")
-info_dict = geolocator.geocode(input_destination + " ,Singapore").raw
-my_lan_lon = [float(info_dict['lat']), float(info_dict['lon'])]
 
-#! step1: 获取目的地周围的
-st.write("### 1.Find Entertainment places nearby")
-tp = st.selectbox("Place type", df['type'].unique())
 
-num = st.selectbox("No. of closest places(Less than 10)", range(1,11))
+# step0的处理目的地输入异常
+try:
+    input_destination = st.text_input("A place name in SG, PRESS Enter!")
+    info_dict = geolocator.geocode(input_destination + " , Singapore").raw
+except AttributeError as e:
+    st.write(f"### Note: Your input place \"{input_destination}\" cannot \
+                    be decoded in Sinapore! Try another please.")
+else: # Input destination is correct, then show rest...
 
-# 有输入了，再生成DF+画图
-if input_destination:
-    # 需要根据位置坐标，排序
-    st.write(f"Your place geolocation is {[round(num, 3) for num in my_lan_lon]}, \
-        {num} closest {tp} is/are:")
 
-    # filter by type
-    df_with_type = df.loc[df.type == tp]
+    my_lan_lon = [float(info_dict['lat']), float(info_dict['lon'])]
 
-    # 计算离my_loc的距离
-    dis2me = [] # add a new colm
-    for idx, row in df_with_type.iterrows():
-        dis2me.append(geopy.distance.distance(my_lan_lon, [row.lat, row.lon]).km)
-    df_with_type.loc[:, 'dis2me'] = dis2me # add a new col
-    # st.write(df_with_type)
+    #! step1: 获取目的地周围的
+    st.write("### 1.Find Entertainment places nearby")
+    tp = st.selectbox("Place type", df['type'].unique())
 
-    # 获取K个最近的地点
-    df_K_closest = df_with_type.nsmallest(num, 'dis2me')
-    df_K_closest.reset_index(drop=True, inplace=True)
-    st.write(df_K_closest.drop('type', axis=1))
+    num = st.selectbox("No. of closest places(Less than 10)", range(1,11))
 
-    # 同时，一行new row 加入my location，一起做转化和可视化在图上
-    df_K_closest.loc[-1] = [input_destination, info_dict['display_name'], '', my_lan_lon[0], my_lan_lon[1], 0.0]
+    # 有输入了，再生成DF+画图
+    if input_destination:
+        # 需要根据位置坐标，排序
+        st.write(f"Your place geolocation is {[round(num, 3) for num in my_lan_lon]}, \
+            {num} closest {tp} is/are:")
 
-    # ------------------------Boken 画图准备-----------------------
+        # filter by type
+        df_with_type = df.loc[df.type == tp]
 
-    # Define coord as tuple (lat,long)
-    df_K_mercat = df_K_closest.copy(deep=True)
-    df_K_mercat['coordinates'] = list(zip(df_K_mercat['lat'], df_K_mercat['lon']))
+        # 计算离my_loc的距离
+        dis2me = [] # add a new colm
+        for idx, row in df_with_type.iterrows():
+            dis2me.append(geopy.distance.distance(my_lan_lon, [row.lat, row.lon]).km)
+        df_with_type.loc[:, 'dis2me'] = dis2me # add a new col
+        # st.write(df_with_type)
 
-    # Obtain list of mercator coordinates
-    mercators = [x_coord(x, y) for x, y in df_K_mercat['coordinates']]
-    # Create mercator column in our df_K_closest
-    df_K_mercat['mercator'] = mercators
-    # Split that column out into two separate columns - mercator_x and mercator_y
-    df_K_mercat[['mercator_x', 'mercator_y']] = df_K_mercat['mercator'].apply(pd.Series)
-    # Select tile set to use
-    chosentile = get_provider(Vendors.STAMEN_TONER)
-    # Choose palette
-    palette = PRGn[10]
+        # 获取K个最近的地点
+        df_K_closest = df_with_type.nsmallest(num, 'dis2me')
+        df_K_closest.reset_index(drop=True, inplace=True)
+        st.write(df_K_closest.drop('type', axis=1))
 
-    # Set tooltips - these appear when we hover over a data point in our map, very nifty and very useful
-    tooltips = [("Place Name","@name"), ("Address", "@address")]
+        # 同时，一行new row 加入my location，一起做转化和可视化在图上
+        df_K_closest.loc[-1] = [input_destination, info_dict['display_name'], '', my_lan_lon[0], my_lan_lon[1], 0.0]
 
-    # Create figure
-    p = figure(title = 'Places@Singapore', x_axis_type="mercator", y_axis_type="mercator",
-            x_axis_label = 'Longitude', y_axis_label = 'Latitude', tooltips = tooltips,
-            plot_width=900, plot_height=800)
+        # ------------------------Boken 画图准备-----------------------
 
-    # Add map tile
-    p.add_tile(chosentile)
-    df_myloc = ColumnDataSource(data=df_K_mercat.iloc[-1:])
-
-    # 始终显示 the point for my location
-    p.inverted_triangle(x='mercator_x', y='mercator_y',
-            source=df_myloc, size=25, fill_alpha=1, color='red')
-#-------------------准备end--------------------
-
-    #选择地点并获取附近停车场
-    with open("data/carpark_coordinates.json") as fin:
-        coordinates = json.load(fin)
-    with open("data/closest_parking_lot.json") as fin:
-        closest_parking_lot = json.load(fin)
-
-    # 获取用户选择的目的地
-    st.write("### 2.Choose a Place, then Carpark info will show up.")
-    df_K_closest = df_K_closest.iloc[:-1]
-    # dest_name = df_K_closest.name[0]
-    dest_name = st.selectbox(f"Choose a destination from the {num} nearest places above",
-                            pd.Series(" ").append(df_K_closest.name))
-    # dest_info = df_K_closest.loc[df_K_closest['name']==dest_name]
-
-    ###########################################################
-    # pre-call this function to save time
-    # THIS IS TIME CONSUMING. DO NOT CALL THIS TOO FREQUENTLY.
-    available_parking_lots = get_current_parking_data()
-    ###########################################################
-
-    # ------------------判定显示：选定的places VS 停车场-----------------
-    if dest_name == " ": # 第一幅图
-        df_places = ColumnDataSource(data=df_K_mercat.iloc[:-1])
-
-        p.circle(x='mercator_x', y='mercator_y',
-                source=df_places, size=15, fill_alpha=.7)
-    
-    else: # 第二幅图，停车信息
-        st.write(f"You choose {dest_name} as destination(<font color=‘blue’>blue traingle</font>)!", unsafe_allow_html=True)
-        st.write("The Parking info are shown as above(<font color=green>green circle</font>):", unsafe_allow_html=True)
-        
-        # 获取目的地经纬度
-        dest_lat = df_K_closest.loc[df_K_closest['name']==dest_name].iat[0,3]
-        dest_lon = df_K_closest.loc[df_K_closest['name']==dest_name].iat[0,4]
-
-        # 找到附近的停车场
-        # closest_carpark = closest_parking_lot[dest_name]
-        # use new version
-        closest_carpark = NV_find_closest_N_carpark(dest_lat, dest_lon, N=5, carpark_coord=coordinates) 
-
-        # 展示附近停车场信息
-        carpark_info = []
-        for i in closest_carpark:
-            distance = distance_from_dest(dest_lat, dest_lon, coordinates[i][0], coordinates[i][1])
-            # new_row = [i,coordinates[i][0],coordinates[i][1],check_availability([i])[0],distance]
-            # new_row = [i,[coordinates[i][0],coordinates[i][1]],coordinates[i][2],check_availability([i])[0],distance]
-            new_row = [i, [coordinates[i][0], coordinates[i][1]], coordinates[i][2], check_availability(i, available_parking_lots), distance] # input of check_availability has been redesigned
-            carpark_info.append(new_row)
-        df_carpark = pd.DataFrame(carpark_info)
-        # df_carpark.columns = ["carpark_number", "lat", "lon", "number_of_available_lots", "distance_from_dest"]
-        df_carpark.columns = ["name", "loc", "address", "number_of_available_lots", "distance_from_dest (km)"]
-
-        st.write(df_carpark.drop('loc', axis=1))
-        # st.write(df_K_mercat)
+        # Define coord as tuple (lat,long)
+        df_K_mercat = df_K_closest.copy(deep=True)
+        df_K_mercat['coordinates'] = list(zip(df_K_mercat['lat'], df_K_mercat['lon']))
 
         # Obtain list of mercator coordinates
-        carpark_mercators = [x_coord(x, y) for x, y in df_carpark['loc']]
+        mercators = [x_coord(x, y) for x, y in df_K_mercat['coordinates']]
         # Create mercator column in our df_K_closest
-        df_carpark['mercator'] = carpark_mercators
+        df_K_mercat['mercator'] = mercators
         # Split that column out into two separate columns - mercator_x and mercator_y
-        df_carpark[['mercator_x', 'mercator_y']] = df_carpark['mercator'].apply(pd.Series)
+        df_K_mercat[['mercator_x', 'mercator_y']] = df_K_mercat['mercator'].apply(pd.Series)
+        # Select tile set to use
+        chosentile = get_provider(Vendors.STAMEN_TONER)
+        # Choose palette
+        palette = PRGn[10]
 
-        # 绘制选定destination—— 用, 蓝色正三角
-        df_choose_places = ColumnDataSource(data=df_K_mercat.loc[df_K_mercat['name']==dest_name])
-        p.triangle(x='mercator_x', y='mercator_y',
-                    source=df_choose_places, size=20, fill_alpha=1)
+        # Set tooltips - these appear when we hover over a data point in our map, very nifty and very useful
+        tooltips = [("Place Name","@name"), ("Address", "@address")]
 
-        # 选定destination 附近停车场地点, 画图 —— 绿色点
-        df_closest_carpark = ColumnDataSource(data=df_carpark)
-        p.circle(x='mercator_x', y='mercator_y',
-                source=df_closest_carpark, size=15, fill_alpha=.7, color="green")
+        # Create figure
+        p = figure(title = 'Places@Singapore', x_axis_type="mercator", y_axis_type="mercator",
+                x_axis_label = 'Longitude', y_axis_label = 'Latitude', tooltips = tooltips,
+                plot_width=900, plot_height=800)
+
+        # Add map tile
+        p.add_tile(chosentile)
+        df_myloc = ColumnDataSource(data=df_K_mercat.iloc[-1:])
+
+        # 始终显示 the point for my location
+        p.inverted_triangle(x='mercator_x', y='mercator_y',
+                source=df_myloc, size=25, fill_alpha=1, color='red')
+    #-------------------准备end--------------------
+
+        #选择地点并获取附近停车场
+        with open("data/carpark_coordinates.json") as fin:
+            coordinates = json.load(fin)
+        with open("data/closest_parking_lot.json") as fin:
+            closest_parking_lot = json.load(fin)
+
+        # 获取用户选择的目的地
+        st.write("### 2.Choose a Place, then Carpark info will show up.")
+        df_K_closest = df_K_closest.iloc[:-1]
+        # dest_name = df_K_closest.name[0]
+        dest_name = st.selectbox(f"Choose a destination from the {num} nearest places above",
+                                pd.Series(" ").append(df_K_closest.name))
+        # dest_info = df_K_closest.loc[df_K_closest['name']==dest_name]
+
+        ###########################################################
+        # pre-call this function to save time
+        # THIS IS TIME CONSUMING. DO NOT CALL THIS TOO FREQUENTLY.
+        available_parking_lots = get_current_parking_data()
+        ###########################################################
+
+        # ------------------判定显示：选定的places VS 停车场-----------------
+        if dest_name == " ": # 第一幅图
+            df_places = ColumnDataSource(data=df_K_mercat.iloc[:-1])
+
+            p.circle(x='mercator_x', y='mercator_y',
+                    source=df_places, size=15, fill_alpha=.7)
+        
+        else: # 第二幅图，停车信息
+            st.write(f"You choose {dest_name} as destination(<font color=‘blue’>blue traingle</font>)!", unsafe_allow_html=True)
+            st.write("The Parking info are shown as above(<font color=green>green circle</font>):", unsafe_allow_html=True)
+            
+            # 获取目的地经纬度
+            dest_lat = df_K_closest.loc[df_K_closest['name']==dest_name].iat[0,3]
+            dest_lon = df_K_closest.loc[df_K_closest['name']==dest_name].iat[0,4]
+
+            # 找到附近的停车场
+            # closest_carpark = closest_parking_lot[dest_name]
+            # use new version
+            closest_carpark = NV_find_closest_N_carpark(dest_lat, dest_lon, N=5, carpark_coord=coordinates) 
+
+            # 展示附近停车场信息
+            carpark_info = []
+            for i in closest_carpark:
+                distance = distance_from_dest(dest_lat, dest_lon, coordinates[i][0], coordinates[i][1])
+                # new_row = [i,coordinates[i][0],coordinates[i][1],check_availability([i])[0],distance]
+                # new_row = [i,[coordinates[i][0],coordinates[i][1]],coordinates[i][2],check_availability([i])[0],distance]
+                new_row = [i, [coordinates[i][0], coordinates[i][1]], coordinates[i][2], check_availability(i, available_parking_lots), distance] # input of check_availability has been redesigned
+                carpark_info.append(new_row)
+            df_carpark = pd.DataFrame(carpark_info)
+            # df_carpark.columns = ["carpark_number", "lat", "lon", "number_of_available_lots", "distance_from_dest"]
+            df_carpark.columns = ["name", "loc", "address", "number_of_available_lots", "distance_from_dest (km)"]
+
+            st.write(df_carpark.drop('loc', axis=1))
+            # st.write(df_K_mercat)
+
+            # Obtain list of mercator coordinates
+            carpark_mercators = [x_coord(x, y) for x, y in df_carpark['loc']]
+            # Create mercator column in our df_K_closest
+            df_carpark['mercator'] = carpark_mercators
+            # Split that column out into two separate columns - mercator_x and mercator_y
+            df_carpark[['mercator_x', 'mercator_y']] = df_carpark['mercator'].apply(pd.Series)
+
+            # 绘制选定destination—— 用, 蓝色正三角
+            df_choose_places = ColumnDataSource(data=df_K_mercat.loc[df_K_mercat['name']==dest_name])
+            p.triangle(x='mercator_x', y='mercator_y',
+                        source=df_choose_places, size=20, fill_alpha=1)
+
+            # 选定destination 附近停车场地点, 画图 —— 绿色点
+            df_closest_carpark = ColumnDataSource(data=df_carpark)
+            p.circle(x='mercator_x', y='mercator_y',
+                    source=df_closest_carpark, size=15, fill_alpha=.7, color="green")
 
 
-    st.write(p)
+        st.write(p)
 
 
 
